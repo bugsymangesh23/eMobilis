17fde000752b66fe7dfcd9ea6d2496af6b8be8f5
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-11-09 16:15:37 +0300

    lesson_7

---
+++ Python/lesson_7.py
@@ -0,0 +1,29 @@
+# Dictionary
+# list [], tuple (), dictionary {}
+student = {"name": "Jane Sarah", "id": 1234, "age": 21, "gender": "F"}
+
+print(student["name"]) # key
+print(student)
+
+student["weight"] = 61
+
+print(student)
+
+# set -- only one existence per item, unordered,mutable
+people = {"Jane", "Bill", "Kevo", "Said", "Jane"}
+print(people)
+people.add("Willy")
+print(people)
+print(len(people)) # count
+people.discard("Kevo")
+print(people)
+
+# git init
+# git add .
+# git commit -m "Python basics"
+# git remote add origin https://github.com/coolmindcodes/python_basics.git
+# git push -u origin master
+
+
+
+# loops and functions in python
