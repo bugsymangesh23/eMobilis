6175e1b5b27408e9b488091d002b6daa3438515b
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-11-09 16:14:46 +0300

    lesson_6

---
+++ Python/lesson_6.py
@@ -0,0 +1,13 @@
+
+# Tuples
+# immutable -- CANNOT CHANGE
+
+days_of_the_week = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+
+print(days_of_the_week[0])
+print(days_of_the_week)
+
+print(len(days_of_the_week))
+
+people = ("Julie",) # single value
+
